ebe4f037911bb0ce01409f91d69bba1b3c4971ea
Adrien Chardon <adrienchardon@mailoo.org>
2022-08-24 21:42:40 +0200

    main: update date range

--- main.py
+++ main.py
@@ -11,8 +11,8 @@ from process import process_mileage, process_speed, process_predict_times
 #START = pd('2020-11-23')  # Monday
 #START = pd('2021-03-22')  # Monday
 START = pd('2022-01-03')  # Monday
-MID = pd("2022-10-15")
-END = pd("2023-05-01")
+MID = pd("2022-11-30")
+END = pd("2023-04-30")
 
 
 START_WEEKLY = START.replace(day=START.day-START.weekday())
